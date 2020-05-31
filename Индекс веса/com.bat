e0eefb474fb87a4c020f7b418aa182d3ec1ad172
Artur <57113088+KsyDark@users.noreply.github.com>
2020-05-31 19:31:35 +0300

    Update com.bat

--- Индекс веса/com.bat
+++ Индекс веса/com.bat
@@ -1,6 +1,6 @@
 :: Запускает комплицию проекта с помощью pyinstaller
 :: Предварительно установите кодировку - Кирилица OEM 866
-pyinstaller --onefile --noconsole --icon=ves.ico "Расчёт коэффициента веса.py"
+pyinstaller --onefile --noconsole --icon=ves.ico "Индекс веса.py"
 :: Запускаем программу после компиляции
 cd dist
 start "Расчёт" "Индекс веса.exe"
